7dbaf98750f84475e2d23ae6550022b1909d4740
archi <aidamatrix@gmail.com>
2022-04-25 19:00:55 +0300

    sight_list_screen.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:places/ui/screen/sight_list_screen.dart';
 
 void main() {
   runApp(const App());
@@ -54,7 +55,7 @@ class App extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.grey,
       ),
-      home: const MyFirstStfulWidget(),
+      home: const SightListScreen(),
     );
   }
 }
